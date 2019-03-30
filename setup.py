2d16470fc7ecbfbd9f32dc19904070de61e55250
Chintalagiri Shashank <shashank.chintalagiri@gmail.com>
2019-03-31 01:11:03 +0530

    Bumped version

--- setup.py
+++ setup.py
@@ -14,7 +14,8 @@ with open('README.rst') as readme_file:
     readme = readme_file.read()
 
 requirements = [
-    'tendril-utils-core>=0.1.4',
+    'tendril-utils-core>=0.1.9',
+    'tendril-config>=0.1.1',
     'six',
     'lxml',
     'beautifulsoup4',
@@ -30,7 +31,7 @@ test_requirements = [
 
 setup(
     name='tendril-utils-www',
-    version='0.1.2',
+    version='0.1.3',
     description="Internet utilities for tendril",
     long_description=readme,
     author="Chintalagiri Shashank",
